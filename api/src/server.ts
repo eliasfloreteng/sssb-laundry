112200f52acb46dc57dc45bdea0bb8cb1f0d1e1b
Elias <18127101+eliasfloreteng@users.noreply.github.com>
2026-04-28 22:58:54 +0200

    Fix max param length

--- api/src/server.ts
+++ api/src/server.ts
@@ -17,7 +17,9 @@ interface TimeslotParams {
 export function buildServer(args?: { aptusClient?: AptusClient }): FastifyInstance {
   const app = Fastify({
     logger: true,
-    maxParamLength: 256
+    routerOptions: {
+      maxParamLength: 256
+    }
   });
   const aptus = args?.aptusClient ?? new AptusClient();
 
